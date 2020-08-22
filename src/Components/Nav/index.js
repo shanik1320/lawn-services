e7b14bbcf1a3f29d857645fba0776fe55048ddc4
shanik hernandez <shaniik.her93@gmail.com>
2020-08-22 10:35:33 -0400

    hid search bar and added Contact us link

--- src/Components/Nav/index.js
+++ src/Components/Nav/index.js
@@ -12,11 +12,12 @@ function Navigation () {
           <Nav.Link href="#home">Home</Nav.Link>
           <Nav.Link href="#services">Services</Nav.Link>
           <Nav.Link href="#pricing">Pricing</Nav.Link>
+          <Nav.Link href="#contact">Contact Us</Nav.Link>
         </Nav>
-        <Form inline>
+        {/* <Form inline>
           <FormControl type="text" placeholder="Search" className="mr-sm-2" />
           <Button variant="outline-info">Search</Button>
-        </Form>
+        </Form> */}
       </Navbar>
     )
 }
