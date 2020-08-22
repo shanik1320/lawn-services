87b8d81fc4b1cb085f3dfa10fc886e94071d5fd7
shanik hernandez <shaniik.her93@gmail.com>
2020-08-22 10:23:09 -0400

    Nav imported in App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import Nav from "./components/Nav"
+import Nav from "./Components/Nav/index"
 
 function App() {
   return (
     <div>
-      
+      <Nav/>
     </div>
   );
 }
