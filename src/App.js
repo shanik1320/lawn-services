4abdd7b10ae043fb64cb373f61b92c7804a7da32
shanik hernandez <shaniik.her93@gmail.com>
2020-08-22 10:18:50 -0400

    deleted components file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import Navigation from "./components/Navigation"
+import Nav from "./components/Nav"
 
 function App() {
   return (
     <div>
-      <Navigation/>
+      
     </div>
   );
 }
