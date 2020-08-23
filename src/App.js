fb6752ebaf363b089b1a5eb74b95eceb316e4371
shanik hernandez <shaniik.her93@gmail.com>
2020-08-23 12:11:09 -0400

    picture added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Nav from "./Components/Nav/index";
 import Footer from "./Components/Footer/index";
+// import "./App.css"
 
 function App() {
   return (
--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -3,12 +3,12 @@ import { Container } from "react-bootstrap";
 
 function Footer() {
   return (
-      <Container style={{position:"fixed", bottom: "0", backgroundColor:"black" }}>
+      <div style={{position:"fixed", bottom: "0", backgroundColor:"black" }}>
     <footer>
      
          <p style={{color:"white", }} >Lawn Care because you Care</p>
     </footer>
-    </Container>
+    </div>
   ); 
 }
 
