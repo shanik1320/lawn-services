4f9706601a970b80ec3f5b7069062634ae8a6173
shanik hernandez <shaniik.her93@gmail.com>
2020-08-23 12:26:59 -0400

    worked on the footer

--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -3,12 +3,12 @@ import { Container } from "react-bootstrap";
 
 function Footer() {
   return (
-      <div style={{position:"fixed", bottom: "0", backgroundColor:"black" }}>
+      <Container style={{position:"fixed", bottom: "0", backgroundColor:"black" }}>
     <footer>
      
-         <p style={{color:"white", }} >Lawn Care because you Care</p>
+         <p style={{color:"white", textAlign:"center", fontFamily:"sans-serif", fontSize:"20px" }} >Lawn Care because We Care</p>
     </footer>
-    </div>
+    </Container>
   ); 
 }
 
